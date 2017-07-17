982b00a82c0e40d1f64572448dc2e6504af1bc07
raa2 <raa2@illinois.edu>
2017-07-17 16:09:26 -0500

    Move image to right block

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
 		<?php } ?>
 		</div>
 		<div class="logo"><?php the_custom_logo(); ?></div>
-		<a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><a href="https://pb-sandbox.library.illinois.edu" class="iopn_brand"><img src="<?php bloginfo( 'template_url' ); ?>/images/iopn_color.png" alt="pressbooks-branding"/> <span><?php _e; ?></span></a><?php bloginfo( 'name' ); ?></a>
+		<div class="right-block"><a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><a href="https://pb-sandbox.library.illinois.edu" class="iopn_brand"><img src="<?php bloginfo( 'template_url' ); ?>/images/iopn_color.png" alt="pressbooks-branding"/></a> <span><?php _e; ?></span><?php bloginfo( 'name' ); ?></a></div>
 		<h2><?php bloginfo( 'description' ); ?></h2>
 		<?php if ( get_theme_mod( 'pressbooks_publisher_intro_textbox' ) !== '' ) { ?>
 			<?php if ( 'one-column' == get_theme_mod( 'pressbooks_publisher_intro_text_col' ) ) { ?>
