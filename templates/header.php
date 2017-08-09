b33f0fb1f900486928d76674831cac9751084c88
raa2 <raa2@illinois.edu>
2017-08-09 15:48:58 -0500

    header image test2.3

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,8 @@
 <header class="banner">
 	<div><h1><a href="https://pb-sandbox.library.illinois.edu"><img 
-	align="left" src=="<?php echo get_template_directory_uri(); ?>/wordpress/wp-content/themes/pressbooks-book/assets/images/iopn_color.png" 
+	align="left" src=="<?php echo get_template_directory_uri(); ?>/pressbooks-book/assets/images/iopn_color.png" 
 	alt="IOPN Network"></img>
-	<img align="left" src=="<?php echo get_template_directory_uri(); ?>/wordpress/wp-content/themes/pressbooks-publisher/assets/images/ui_library.png" 
+	<img align="left" src=="<?php echo get_template_directory_uri(); ?>/pressbooks-publisher/assets/images/ui_library.png" 
 	alt="university branding"></img></a></h1></div>
   <div class="container">
     <div class="link-wrap">
